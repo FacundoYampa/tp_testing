82e0d5b845fb110777d88e252041f0d7813da396
FacundoYampa <facundoyampa@gmail.com>
2024-05-22 18:18:52 -0300

    avance validaciones

--- js/main.js
+++ js/main.js
@@ -10,37 +10,64 @@ $(document).ready(function()
                         var nombre = $('#Nombre').val();
                         var apellido = $('#Apellido').val();
                         var email = $('#Email').val();
+                        var edad = $('#Edad').val();
 
-                        var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;//regex para email en una variable
+                        let valido = true;
+
+                        var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;  //regex para email en una variable
+                        var soloTextoRegex = /^[a-zA-Z\s]+$/;           //regex para que entre solo texto
+                        var numRegex = /^\d+$/;                         //regex para numeros enteros
 
                         //Validación front
-                        if(nombre==""|apellido==""|email==""){
+                        if(nombre==""||apellido==""||email==""||edad==""){
                             event.preventDefault();
-                            
+                            valido = false;
                             const toastLiveExample = document.getElementById('liveToast');
                             const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample);
                             toastBootstrap.show();
                         }
 
+                        else if(!soloTextoRegex.test(nombre)||nombre.length>51){
+                            event.preventDefault();
+                            valido = false;
+                            alert("el nombre debe contener solo letras")
+                        }
+
+                        else if(!soloTextoRegex.test(apellido)||nombre.length>51){
+                            event.preventDefault();
+                            valido = false;
+                            alert("el apellido debe contener solo letras")
+                        }
+
+
                         else if(!emailRegex.test(email))//metodo para validar la expresion regular
                         {
                             event.preventDefault();
-                            
+                            valido = false;
                             const toastLiveExample = document.getElementById('liveToastMail');
                             const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample);
-                            toastBootstrap.show();
+                            toastBootstrap.show(); //podria usar una funcion y cambiar parametros
                         }
-                        else
+                        else if(!numRegex.test(edad)||edad<18||edad>120)
                         {
+                            event.preventDefault();
+                            valido = false;
+                            // const toastLiveExample = document.getElementById('liveToastMail');
+                            // const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample);
+                            // toastBootstrap.show();
+                            alert("La edad debe ser un nuero entero entre 18 y 120 ")
+                        }
+
+                        else if(valido = true)
+                        {
+                            alert("Se envió correctamente");
                             $.ajax
                             (
                                 {
                                     url:'insertar.php',
                                     method:'post',
-                                    data:{Uname:nombre,Uapellido:apellido,Uemail:email},
-                                    success:function(data){
-                                        alert("Se envió correctamente");
-                                    },
+                                    data:{Uname:nombre,Uapellido:apellido,Uemail:email,Uedad:edad},
+                                    success:function(){alert("Se envió correctamente:");},
                                     error:function(){alert("Error de conexión con el servidor");}
                                 }
                             )
